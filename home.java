66824c9c0ceba0756fb7bf11047bbbf895366ef7
Harshitha <harshitharesu2002@gmail.com>
2026-03-15 06:22:59 +0000

    modified home page

---
+++ home.java
@@ -0,0 +1,11 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 60;
+    int second =70;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }}
