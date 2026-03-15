32f330a2cb218f07f049b288c92fae32960d1922
Harshitha <harshitharesu2002@gmail.com>
2026-03-15 06:00:31 +0000

    reg page

--- Registration.java
+++ Registration.java
@@ -1,2 +1,12 @@
-This allows a new user to register by entering the details
-ls
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// A simple class to represent a User
+class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
---
+++ home.java
@@ -0,0 +1,1 @@
+public class Home { ... }: This defines a class named Home. The file name must be Home.java.
