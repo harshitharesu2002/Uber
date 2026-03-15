36fbd47ce36bfd411a65b1e2436c05a512f2e642
Harshitha <harshitharesu2002@gmail.com>
2026-03-15 05:16:56 +0000

    print statement

--- login.java
+++ login.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // The line below prints "Hello, World!" to the standard output
-        System.out.println("Hello, World!");
+        System.out.println("Hi all welcome to AWS and Devops");
     }
 }
